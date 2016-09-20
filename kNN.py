4bfab7bf32fb95f166acfded11a15740e88aaa84
littleeleventhwolf <2367785195@qq.com>
2016-09-20 22:35:40 +0800

    add classifier testing code for dating site

--- kNN.py
+++ kNN.py
@@ -71,3 +71,20 @@ def autoNorm(dataSet):
     # Element-wise division
     normDataSet = normDataSet/tile(ranges, (m, 1))
     return normDataSet, ranges, minVals
+
+# classifier testing code for dating site
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], \
+                                     datingLabels[numTestVecs:m], 3)
+        print "the classifier came back with: %d, the real answer is: %d" \
+              % (classifierResult, datingLabels[i])
+        if(classifierResult != datingLabels[i]): errorCount += 1.0
+    print "the total error rate is %f" % (errorCount/float(numTestVecs))
+
