d74805d0714807a3e9d3fef4b949d2b47e1bdfd9
littleeleventhwolf <2367785195@qq.com>
2016-09-20 23:06:25 +0800

    add method img2vector(): converting images into test vectors

--- kNN.py
+++ kNN.py
@@ -103,3 +103,13 @@ def classifyPerson():
     print "you will probably like this person: ", \
           resultList[classifierResult - 1]
 
+# converting images into test vectors
+def img2vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32*i+j] = int(lineStr[j])
+    return returnVect
+
