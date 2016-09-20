6a3d6fb51969525a4288f54a1c0ab9945afe71de
littleeleventhwolf <2367785195@qq.com>
2016-09-20 23:34:00 +0800

    add handwritten digits testing code

--- kNN.py
+++ kNN.py
@@ -1,3 +1,4 @@
+from os import listdir
 from numpy import *
 import operator
 
@@ -112,4 +113,35 @@ def img2vector(filename):
         for j in range(32):
             returnVect[0, 32*i+j] = int(lineStr[j])
     return returnVect
+
+# handwritten digits testing code
+def handwritingClassTest():
+    hwLabels = []
+    # get contents of directory
+    trainingFileList = listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        # process class num from filename
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, \
+                                     trainingMat, hwLabels, 3)
+        print "the classifier came back with: %d, the real answer is: %d" \
+              % (classifierResult, classNumStr)
+        if(classifierResult != classNumStr): errorCount += 1.0
+    print "\nthe total number of errors is: %d" % errorCount
+    print "\nthe total error rate is: %f" % (errorCount/float(mTest))
 
