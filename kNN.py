33921505b7728815ee8206dd6673e959034abb14
littleeleventhwolf <2367785195@qq.com>
2016-09-20 21:34:19 +0800

    add method file2matrix()

--- kNN.py
+++ kNN.py
@@ -41,3 +41,23 @@ def classify0(inX, dataSet, labels, k):
                               reverse=True)
     return sortedClassCount[0][0]
 
+# transfer data from file to matrix
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    # get number of lines in file
+    numberOfLines = len(arrayOLines)
+    # create numpy matrix to return
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
+    # parse line to a list
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
+
