10fce1729141e1ec43a60b5dc5d03abbdb81bdb2
littleeleventhwolf <2367785195@qq.com>
2016-09-21 15:54:36 +0800

    add method createDataSet(): the simple data about fish identification

--- decision-trees/trees.py
+++ decision-trees/trees.py
@@ -16,3 +16,15 @@ def calcShannonEnt(dataSet):
         prob = float(labelCounts[key])/numEntries
         shannonEnt -= prob * log(prob, 2)
     return shannonEnt
+
+# simple data about fish identification
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+
