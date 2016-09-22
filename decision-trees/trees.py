b34f16191559b19454ea6f5b0b1c3637d7687919
littleeleventhwolf <2367785195@qq.com>
2016-09-22 09:48:55 +0800

    add methods for persisting the decision tree with pickle

--- decision-trees/trees.py
+++ decision-trees/trees.py
@@ -128,3 +128,15 @@ def classify(inputTree, featLabels, testVec):
 			else:
 				classLabel = secondDict[key]
 	return classLabel
+
+# methods for persisting the decision tree with pickle
+def storeTree(inputTree, filename):
+	import pickle
+	fw = open(filename, 'w')
+	pickle.dump(inputTree, fw)
+	fw.close()
+
+def grabTree(filename):
+	import pickle
+	fr = open(filename)
+	return pickle.load(fr)
