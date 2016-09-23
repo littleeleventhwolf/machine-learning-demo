df854d82d8d08a0ecaa3a64f77e89174e4305d09
littleeleventhwolf <2367785195@qq.com>
2016-09-23 12:28:59 +0800

    add naive bayes classifier function

--- naive-bayes/bayes.py
+++ naive-bayes/bayes.py
@@ -1,3 +1,5 @@
+from numpy import *
+
 # word list to vector function
 def loadDataSet():
 	postingList = [['my', 'dog', 'has', 'flea', \
@@ -27,3 +29,29 @@ def setOfWords2Vec(vocabList, inputSet):
 		else:
 			print "the word: %s is not in my Vocabulary!" % word
 	return returnVec
+
+# naive bayes classifier training function
+def trainNBO(trainMatrix, trainCategory):
+	numTrainDocs = len(trainMatrix)
+	numWords = len(trainMatrix[0])
+	# initialize probability
+	pAbusive = sum(trainCategory)/float(numTrainDocs)
+	p0Num = zeros(numWords)
+	p1Num = zeros(numWords)
+	p0Denom = 0.0
+	p1Denom = 0.0
+
+	for i in range(numTrainDocs):
+		if trainCategory[i] == 1:
+			# vector addition
+			p1Num += trainMatrix[i]
+			p1Denom += sum(trainMatrix[i])
+		else:
+			p0Num += trainMatrix[i]
+			p0Denom += sum(trainMatrix[i])
+			
+	# element-wise division
+	p1Vect = p1Num / p1Denom # change to log()
+	p0Vect = p0Num / p0Denom # change to log()
+	return p0Vect, p1Vect, pAbusive
+
