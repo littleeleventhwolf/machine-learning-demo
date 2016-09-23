77e1bbd5abf3b0c2bac686ba329f3a65afa1cc9d
littleeleventhwolf <2367785195@qq.com>
2016-09-23 16:01:53 +0800

    add file parsing and full spam test functions

--- naive-bayes/bayes.py
+++ naive-bayes/bayes.py
@@ -88,3 +88,47 @@ def testingNB():
 	testEntry = ['stupid', 'garbage']
 	thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
 	print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
+
+# Classifying spam email with naive bayes
+
+# file parsing and full spam test functions
+def textParse(bigString):
+	import re
+	listOfTokens = re.split(r'\W*', bigString)
+	return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+def spamTest():
+	docList = []
+	classList = []
+	fullText = []
+	for i in range(1, 26):
+		# load and parse text files
+		wordList = textParse(open('email/spam/%d.txt' % i).read())
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(1)
+		wordList = textParse(open('email/ham/%d.txt' % i).read())
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(0)
+	vocabList = createVocabList(docList)
+	tainingSet = range(50)
+	testSet = []
+	# randomly create the training set
+	for i in range(10):
+		randIndex = int(random.uniform(0, len(trainingSet)))
+		testSet.append(trainingSet[randIndex])
+		del(trainingSet[randIndex])
+	trainMat = []
+	trainClasses = []
+	for docIndex in trainingSet:
+		trainMat.append(setOfWords2Vec(vocabList, docList[docIndex]))
+		trainClasses.append(classList[docIndex])
+	p0V, p1V, pSpam = trainNBO(array(trainMat), array(trainClasses))
+	errorCount = 0
+	# classify the test set
+	for docIndex in testSet:
+		wordVector = setOfWords2Vec(vocabList, docList[docIndex])
+		if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+			errorCount += 1
+	print 'the error rate is : ', float(errorCount)/len(testSet)
