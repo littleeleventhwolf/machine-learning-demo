ee878b09b7e5a0f9e621e7a000ebfcc8e29c8aa2
littleeleventhwolf <2367785195@qq.com>
2016-09-23 17:25:47 +0800

    add RSS feed classifier and frequent word removal functions

--- naive-bayes/bayes.py
+++ naive-bayes/bayes.py
@@ -135,3 +135,61 @@ def spamTest():
 			print 'classification error ', docList[docIndex]
 			errorCount += 1
 	print 'the error rate is : ', float(errorCount)/len(testSet)
+
+# using naive bayes to reveal local attitudes from person ads
+
+# RSS feed classifier and frequent word removal functions
+def calcMostFreq(vocabList, fullText):
+	"""
+	calculates frequency of occurrence
+	"""
+	import operator
+	freqDict = {}
+	for token in vocabList:
+		freqDict[token] = fullText.count(token)
+	sortedFreq = sorted(freqDict.iteritems(), key=operator.itemgetter(1), \
+		                reverse=True)
+	return sortedFreq[:30]
+
+def localWords(feed1, feed0):
+	import feedparser
+	docList = []
+	classList = []
+	fullText = []
+	minLen = min(len(feed1['entries']), len(feed0['entries']))
+	for i in range(minLen):
+		# accesses one feed at a time
+		wordList = textParse(feed1['entries'][i]['summary'])
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(1)
+		wordList = textParse(feed0['entries'][i]['summary'])
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(0)
+	vocabList = createVocabList(docList)
+	top30Words = calcMostFreq(vocabList, fullText)
+	# removes most frequently occuring words
+	for pairW in top30Words:
+		if pairW[0] in vocabList:
+			vocabList.remove(pairW[0])
+	trainingSet = range(2*minLen)
+	testSet = []
+	for i in range(20):
+		randIndex = int(random.uniform(0, len(trainingSet)))
+		testSet.append(trainingSet[randIndex])
+		del(trainingSet[randIndex])
+	trainMat = []
+	trainClasses = []
+	for docIndex in trainingSet:
+		trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
+		trainClasses.append(classList[docIndex])
+	p0V, p1V, pSpam = trainNBO(array(trainMat), array(trainClasses))
+	errorCount = 0
+	for docIndex in testSet:
+		wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
+		if classifyNB(array(wordVector), p0V, p1V, pSpam) != \
+		          classList[docIndex]:
+		    errorCount += 1
+	print "the error rate is : ", float(errorCount)/len(testSet)
+	return vocabList, p0V, p1V
