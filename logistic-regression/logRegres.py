0874385177a6259d837c7d5e1b6c59ea177de52a
littleeleventhwolf <2367785195@qq.com>
2016-09-24 12:28:15 +0800

    add stochastic gradient ascent and modified version

--- logistic-regression/logRegres.py
+++ logistic-regression/logRegres.py
@@ -13,6 +13,7 @@ def loadDataSet():
 def sigmoid(inX):
 	return 1.0/(1+exp(-inX))
 
+# logistic regression gradient ascent optimization function
 def gradAscent(dataMatIn, classLabels):
 	# convert to numpy matrix data type
 	dataMatrix = mat(dataMatIn)
@@ -26,4 +27,60 @@ def gradAscent(dataMatIn, classLabels):
 		h = sigmoid(dataMatrix * weights)
 		error = (labelMat - h)
 		weights = weights + alpha * dataMatrix.transpose() * error
+	return weights
+
+# plotting the logistic regression best-fit line and dataset
+def plotBestFit(wei):
+	import matplotlib.pyplot as plt
+	weights = wei #.getA()
+	dataMat, labelMat = loadDataSet()
+	dataArr = array(dataMat)
+	n = shape(dataArr)[0]
+	xcord1 = []
+	ycord1 = []
+	xcord2 = []
+	ycord2 = []
+	for i in range(n):
+		if int(labelMat[i]) == 1:
+			xcord1.append(dataArr[i, 1])
+			ycord1.append(dataArr[i, 2])
+		else:
+			xcord2.append(dataArr[i, 1])
+			ycord2.append(dataArr[i, 2])
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+	ax.scatter(xcord2, ycord2, s=30, c='green')
+	x = arange(-3.0, 3.0, 0.1)
+	y = (-weights[0] - weights[1]*x)/weights[2] # best-fit line
+	ax.plot(x, y)
+	plt.xlabel('X1')
+	plt.ylabel('X2')
+	plt.show()
+
+# stochastic gradient ascent
+def stocGradAscent0(dataMatrix, classLabels):
+	m, n = shape(dataMatrix)
+	alpha = 0.01
+	weights = ones(n)
+	for i in range(m):
+		h = sigmoid(sum(dataMatrix[i]*weights))
+		error = classLabels[i] - h
+		weights = weights + alpha * error * dataMatrix[i]
+	return weights
+
+# modified stochastic gradient ascent
+def stocGradAscent1(dataMatrix, classLabels, numIter=150):
+	m, n = shape(dataMatrix)
+	weights = ones(n)
+	for j in range(numIter):
+		dataIndex = range(m)
+		for i in range(m):
+		    alpha = 4/(1.0+j+i)+0.01 # alpha changes with each iteration
+			# update vectors are randomly selected
+		    randIndex = int(random.uniform(0, len(dataIndex)))
+		    h = sigmoid(sum(dataMatrix[randIndex]*weights))
+		    error = classLabels[randIndex] - h
+		    weights = weights + alpha * error * dataMatrix[randIndex]
+		    del(dataIndex[randIndex])
 	return weights
