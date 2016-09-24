c94c4793e8975710d5c6c9408d75df106ad1a549
littleeleventhwolf <2367785195@qq.com>
2016-09-24 10:14:50 +0800

    add logistic regression gradient aascent optimization functions

--- logistic-regression/logRegres.py
+++ logistic-regression/logRegres.py
@@ -1,2 +1,29 @@
 from numpy import *
 
+def loadDataSet():
+	dataMat = []
+	labelMat = []
+	fr = open('testSet.txt')
+	for line in fr.readlines():
+		lineArr = line.strip().split()
+		dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+		labelMat.append(int(lineArr[2]))
+	return dataMat, labelMat
+
+def sigmoid(inX):
+	return 1.0/(1+exp(-inX))
+
+def gradAscent(dataMatIn, classLabels):
+	# convert to numpy matrix data type
+	dataMatrix = mat(dataMatIn)
+	labelMat = mat(classLabels).transpose()
+	m, n = shape(dataMatrix)
+	alpha = 0.001
+	maxCycles = 500
+	weights = ones((n, 1))
+	for k in range(maxCycles):
+		# matrix multiplication
+		h = sigmoid(dataMatrix * weights)
+		error = (labelMat - h)
+		weights = weights + alpha * dataMatrix.transpose() * error
+	return weights
