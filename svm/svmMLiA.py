cbf3023a83ef76ac606a10e9cf047647be170337
littleeleventhwolf <2367785195@qq.com>
2016-09-27 09:54:55 +0800

    add kernel trans function

--- svm/svmMLiA.py
+++ svm/svmMLiA.py
@@ -95,7 +95,7 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
 
 # support functions for full Platt SMO
 class optStruct:
-	def __init__(self, dataMatIn, classLabels, C, toler):
+	def __init__(self, dataMatIn, classLabels, C, toler, kTup):
 		self.X = dataMatIn
 		self.labelMat = classLabels
 		self.C = C
@@ -104,9 +104,12 @@ class optStruct:
 		self.alphas = mat(zeros((self.m, 1)))
 		self.b = 0
 		self.eCache = mat(zeros((self.m, 2))) # error cache
+		self.K = mat(zeros((self.m,self.m)))
+		for i in range(self.m):
+			self.K[:,i] = kernelTrans(self.X, self.X[i,:], kTup)
 
 def calcEk(oS, k):
-	fXk = float(multiply(oS.alphas, oS.labelMat).T*(oS.X*oS.X[k,:].T)) + oS.b
+	fXk = float(multiply(oS.alphas, oS.labelMat).T*oS.K[:,k] + oS.b)
 	Ek = fXk - float(oS.labelMat[k])
 	return Ek
 
@@ -156,7 +159,7 @@ def innerL(i, oS):
 		if L == H:
 			print "L == H"
 			return 0
-		eta = 2.0 * oS.X[i,:]*oS.X[j,:].T - oS.X[i,:]*oS.X[i,:].T - oS.X[j, :]*oS.X[j,:].T
+		eta = 2.0 * oS.K[i, j] - oS.K[i,i] - oS.K[j, j]
 		if eta >= 0:
 			print "eta >= 0"
 			return 0
@@ -168,8 +171,8 @@ def innerL(i, oS):
 			return 0
 		oS.alphas[i] += oS.labelMat[j]*oS.labelMat[i]*(alphaJold - oS.alphas[j])
 		updateEk(oS, i) # update eCache
-		b1 = oS.b - Ei- oS.labelMat[i]*(oS.alphas[i]-alphaIold)*oS.X[i,:]*oS.X[i,:].T - oS.labelMat[j]*(oS.alphas[j]-alphaJold)*oS.X[i,:]*oS.X[j,:].T
-		b2 = oS.b - Ej- oS.labelMat[i]*(oS.alphas[i]-alphaIold)*oS.X[i,:]*oS.X[j,:].T - oS.labelMat[j]*(oS.alphas[j]-alphaJold)*oS.X[j,:]*oS.X[j,:].T
+		b1 = oS.b - Ei- oS.labelMat[i]*(oS.alphas[i]-alphaIold)*oS.K[i,i] - oS.labelMat[j]*(oS.alphas[j]-alphaJold)*oS.K[i,j]
+		b2 = oS.b - Ej- oS.labelMat[i]*(oS.alphas[i]-alphaIold)*oS.K[i,j] - oS.labelMat[j]*(oS.alphas[j]-alphaJold)*oS.K[j,j]
 		if (0 < oS.alphas[i]) and (oS.C > oS.alphas[i]):
 			oS.b = b1
 		elif (0 < oS.alphas[j]) and (oS.C > oS.alphas[j]):
@@ -181,7 +184,7 @@ def innerL(i, oS):
 		return 0
 
 def smoP(dataMatIn, classLabels, C, toler, maxIter,kTup=('lin', 0)):    #full Platt SMO
-    oS = optStruct(mat(dataMatIn),mat(classLabels).transpose(),C,toler)
+    oS = optStruct(mat(dataMatIn),mat(classLabels).transpose(),C,toler, kTup)
     iter = 0
     entireSet = True; alphaPairsChanged = 0
     while (iter < maxIter) and ((alphaPairsChanged > 0) or (entireSet)):
@@ -212,3 +215,49 @@ def calcWs(alphas, dataArr, classLabels):
 		w += multiply(alphas[i]*labelMat[i], X[i,:].T)
 	return w
 
+def kernelTrans(X, A, kTup): 
+    #calc the kernel or transform data to a higher dimensional space
+    m,n = shape(X)
+    K = mat(zeros((m,1)))
+    if kTup[0]=='lin': K = X * A.T   #linear kernel
+    elif kTup[0]=='rbf':
+        for j in range(m):
+            deltaRow = X[j,:] - A
+            K[j] = deltaRow*deltaRow.T
+        K = exp(K/(-1*kTup[1]**2)) #divide in NumPy is element-wise not matrix like Matlab
+    else: raise NameError('Houston We Have a Problem -- That Kernel is not recognized')
+    return K
+
+def testRbf(k1=1.3):
+    dataArr,labelArr = loadDataSet('testSetRBF.txt')
+    b,alphas = smoP(dataArr, labelArr, 200, 0.0001, 10000, ('rbf', k1)) #C=200 important
+    datMat=mat(dataArr); labelMat = mat(labelArr).transpose()
+    svInd=nonzero(alphas.A>0)[0]
+    sVs=datMat[svInd] #get matrix of only support vectors
+    labelSV = labelMat[svInd];
+    print "there are %d Support Vectors" % shape(sVs)[0]
+    m,n = shape(datMat)
+    errorCount = 0
+    for i in range(m):
+        kernelEval = kernelTrans(sVs,datMat[i,:],('rbf', k1))
+        predict=kernelEval.T * multiply(labelSV,alphas[svInd]) + b
+        if sign(predict)!=sign(labelArr[i]): errorCount += 1
+    print "the training error rate is: %f" % (float(errorCount)/m)
+    dataArr,labelArr = loadDataSet('testSetRBF2.txt')
+    errorCount = 0
+    datMat=mat(dataArr); labelMat = mat(labelArr).transpose()
+    m,n = shape(datMat)
+    for i in range(m):
+        kernelEval = kernelTrans(sVs,datMat[i,:],('rbf', k1))
+        predict=kernelEval.T * multiply(labelSV,alphas[svInd]) + b
+        if sign(predict)!=sign(labelArr[i]): errorCount += 1
+    print "the test error rate is: %f" % (float(errorCount)/m)
+
+def img2vector(filename):
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
