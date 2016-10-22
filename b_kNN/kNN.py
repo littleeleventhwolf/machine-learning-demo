ffb435d09d80a96ca04a789efa7a0407b30f3e23
littleeleventhwolf <2367785195@qq.com>
2016-10-22 20:01:40 +0800

    kNN by sklearn

---
+++ b_kNN/kNN.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+
+from sklearn import neighbors
+from sklearn import datasets
+
+knn = neighbors.KNeighborsClassifier()
+
+iris = datasets.load_iris()
+
+# print(iris)
+
+knn.fit(iris.data, iris.target)
+
+predictedLabel = knn.predict([[0.1, 0.2, 0.3, 0.4]])
+
+print(predictedLabel)
