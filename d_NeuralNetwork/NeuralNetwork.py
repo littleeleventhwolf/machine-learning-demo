0f3252e8dd22b9f3345dfecc28a76e918df39048
littleeleventhwolf <2367785195@qq.com>
2016-10-24 20:59:39 +0800

    add some annotation

--- d_NeuralNetwork/NeuralNetwork.py
+++ d_NeuralNetwork/NeuralNetwork.py
@@ -18,22 +18,28 @@ class NeuralNetwork:
 		"""
 		:param layers: A list containing the number of units in each layer. 
 		Should be at least two values.
-		:param activiation: The activiation function to be used. Can be 
+		:param activation: The activation function to be used. Can be 
 		"logistic" and "tanh".
 		"""
-		if activiation == 'logistic':
-			self.activiation = logistic
-			self.activiation_deriv = logistic_derivative
-		elif activiation == 'tanh':
-			self.activiation = tanh
-			self.activiation_deriv = tanh_deriv
+		if activation == 'logistic':
+			self.activation = logistic
+			self.activation_deriv = logistic_derivative
+		elif activation == 'tanh':
+			self.activation = tanh
+			self.activation_deriv = tanh_deriv
 
 		self.weights = []
-		for i in range(1, len(layers)):
+		for i in range(1, len(layers) - 1):
 			self.weights.append((2 * np.random.random((layers[i-1] + 1, layers[i] + 1)) - 1) * 0.25)
 			self.weights.append((2 * np.random.random((layers[i] + 1, layers[i+1])) - 1) * 0.25)
 
 	def fit(self, X, y, learning_rate=0.2, epochs=10000):
+		"""
+		:param X: Dataset instance
+		:param y: Class label
+		:param learning_rate: Length of per step
+		:param epochs: The number of iteration
+		"""
 		X = np.atleast_2d(X)
 		temp = np.ones([X.shape[0], X.shape[1] + 1])
 		temp[:, 0:-1] = X # adding the bias unit to the input layer
@@ -44,18 +50,20 @@ class NeuralNetwork:
 			i = np.random.randint(X.shape[0])
 			a = X[i]
 
-			for l in range(len(self.weights)):
-				a.append(self.activiation(np.dot(a[1], self.weights[l]))) # Compute the node value
+			for l in range(len(self.weights)): # Going forward network, for each layer
+				a.append(self.activation(np.dot(a[l], self.weights[l]))) # Compute the node value
 			error = y[i] - a[-1] # Compute the error at the top layer
-			deltas = [error * self.activiation_deriv(a[-1])] # For output layer
+			deltas = [error * self.activation_deriv(a[-1])] # For output layer
 
 			# Starting backpropagation
-			for l in range(len(a) - 2, 0, -1):
-				deltas.append(deltas[-1].dot(self.weights[l].T)*self.activiation_deriv(a[l]))
+			for l in range(len(a) - 2, 0, -1): # we need to begin at the second to last layer
+				# compute the updated error(i.e. deltas) for each node going from top layer
+				deltas.append(deltas[-1].dot(self.weights[l].T)*self.activation_deriv(a[l]))
 				deltas.reverse()
 				for i in range(len(self.weights)):
 					layer = np.atleast_2d(a[i])
 					delta = np.atleast_2d(deltas[i])
+					# update the weights
 					self.weights[i] += learning_rate * layer.T.dot(delta)
 
 	def predict(self, x):
@@ -64,5 +72,5 @@ class NeuralNetwork:
 		temp[0 : -1] = x
 		a = temp
 		for l in range(0, len(self.weights)):
-			a = self.activiation(np.dot(a, self.weights[l]))
+			a = self.activation(np.dot(a, self.weights[l]))
 		return a
