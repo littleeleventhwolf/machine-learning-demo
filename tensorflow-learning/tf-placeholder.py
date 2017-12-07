983893300a08aaf1632e8cdb0319ddf2cfe558c3
littleeleventhwolf <2367785195@qq.com>
2017-12-07 16:47:31 +0800

    multiple lines placeholder [1 -> 3]

--- tensorflow-learning/tf-placeholder.py
+++ tensorflow-learning/tf-placeholder.py
@@ -4,7 +4,7 @@ w1 = tf.Variable(tf.random_normal([2, 3], stddev=1, seed=1))
 w2 = tf.Variable(tf.random_normal([3, 1], stddev=1, seed=1))
 
 # define placeholder to save input data
-x = tf.placeholder(tf.float32, shape=(1, 2), name="input")
+x = tf.placeholder(tf.float32, shape=(3, 2), name="input")
 a = tf.matmul(x, w1)
 y = tf.matmul(a, w2)
 
@@ -12,4 +12,4 @@ sess = tf.Session()
 init_op = tf.global_variables_initializer()
 sess.run(init_op)
 
-print(sess.run(y, feed_dict={x: [[0.7, 0.9]]}))
+print(sess.run(y, feed_dict={x: [[0.7, 0.9], [0.1, 0.4], [0.5, 0.8]]}))
