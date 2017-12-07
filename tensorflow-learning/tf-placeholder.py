9888efd6c51573e7672c52646b6b2de4b915dee2
littleeleventhwolf <2367785195@qq.com>
2017-12-07 16:29:48 +0800

    tensorflow placeholder demo

---
+++ tensorflow-learning/tf-placeholder.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+w1 = tf.Variable(tf.random_normal([2, 3], stddev=1, seed=1))
+w2 = tf.Variable(tf.random_normal([3, 1], stddev=1, seed=1))
+
+# define placeholder to save input data
+x = tf.placeholder(tf.float32, shape=(1, 2), name="input")
+a = tf.matmul(x, w1)
+y = tf.matmul(a, w2)
+
+sess = tf.Session()
+init_op = tf.global_variables_initializer()
+sess.run(init_op)
+
+print(sess.run(y, feed_dict={x: [[0.7, 0.9]]}))
