df3a9481b1cc20f39466540943b1ccae353ea9dd
littleeleventhwolf <2367785195@qq.com>
2017-12-14 20:19:55 +0800

    tensorflow restore demo

---
+++ tensorflow-learning/tf-restore.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+v1 = tf.Variable(tf.constant(1.0, shape=[1]), name="v1")
+v2 = tf.Variable(tf.constant(2.0, shape=[1]), name="v2")
+result = v1 + v2
+
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+	saver.restore(sess, "./model/model.ckpt")
+	print(sess.run(result))
